7eb98a7ca65853a50a5fe7e594f7865041dd0bf7
Rhuan Coltre <rhuancoltre@gmail.com>
2015-10-19 20:58:27 -0200

    Definição da extensão do passaro, Atualização Camera, Atualização do Pulo do passaro

--- core/src/br/grupointegrado/flappyBird/TelaJogo.java
+++ core/src/br/grupointegrado/flappyBird/TelaJogo.java
@@ -57,12 +57,24 @@ public class TelaJogo extends TelaBase {
         Gdx.gl.glClearColor(.25f, .25f, .25f, 1); // Limpa a tela e pinta a cor de fundo
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Mantem o buffer de cores
 
+        capturaTeclas();
+
+
         atualizar(delta);
         renderizar(delta);
 
 
 
         debug.render(mundo, camera.combined.cpy().scl(Util.PIXEL_METRO));
+    }
+    private boolean pulando = false;
+
+    private void capturaTeclas() {
+
+        pulando = false;
+        if (Gdx.input.justTouched()){
+            pulando = true;
+        }
     }
 
 
@@ -79,8 +91,20 @@ public class TelaJogo extends TelaBase {
      * @param delta
      */
     private void atualizar(float delta) {
+        passaro.atualiar(delta);
         mundo.step(1f / 60f, 6, 2);
-        atualizarChao(delta);
+
+        atualizarCamera();
+        atualizarChao();
+
+        if (pulando) {
+            passaro.pular();
+        }
+    }
+
+    private void atualizarCamera() {
+        camera.position.x = (passaro.getCorpo().getPosition().x + 34 / Util.PIXEL_METRO) * Util.PIXEL_METRO;
+        camera.update();
     }
 
     private void atualizarChao(float delta) {
